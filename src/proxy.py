8caf524bc7c731745aee21c2d9470ab38d59a41c
Sergey Anuchin <sergunich@gmail.com>
2017-04-16 18:53:40 +0300

    Get content from habr

--- src/proxy.py
+++ src/proxy.py
@@ -1,10 +1,16 @@
+import urllib.request
+
 from bottle import request, get, run, debug
 
 @get('<path:path>')
 def proxy(path):
     qs = request.query_string
-    content = path + '?' + qs if qs else path
-    print(content)
+    path_with_qs = path + '?' + qs if qs else path
+    print(path_with_qs)
+
+    with urllib.request.urlopen('https://habrahabr.ru'+path_with_qs) as response:
+       content = response.read()
+
     return content
 
 if __name__ == "__main__":
